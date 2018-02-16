c3c93f30e4fda8d0ff51bf1dc90874ef901ec3f3
Elnaz Esmaeilzadeh Seylabi <elnaz@caltech.edu>
2018-02-16 14:29:59 -0800

    added user.mk file

---
+++ user.mk
@@ -0,0 +1,23 @@
+# System definition
+# -----------------
+
+SYSTEM = XT5
+
+# Main compilation setup
+# ----------------------
+
+CFLAGS += -fastsse -O3
+#CFLAGS += -g -O0 -DDEBUG
+
+# Compilation flags
+# -----------------
+
+# CVM and IO related:
+
+IO_CFLAGS = -DUSECVMDB -DSCEC -DPROCPERNODE=1 -DNO_OUTPUT
+IO_CPPFLAGS = -DUSECVMDB -DSCEC -DPROCPERNODE=1 -DNO_OUTPUT
+
+# Earthquake related
+
+SOLVE_CFLAGS = -DHALFSPACE -DBOUNDARY
+SOLVE_CPPFLAGS = -DHALFSPACE -DBOUNDARY
