5b31b4c19abdba6db5cb7cb4f4f35a0549089b1b
Dorian Restrepo <drestre6@gmail.com>
2019-09-05 10:29:20 -0500

    added Shi & Assimaki (2017) guidelines for tau_max computation for exp model

--- quake/forward/nonlinear.c
+++ quake/forward/nonlinear.c
@@ -1151,20 +1151,30 @@ void nonlinear_solver_init(int32_t myID, mesh_t *myMesh, double depth) {
                 break;
 
             case VONMISES_BAE:
-                //ecp->c         = get_cohesion(elementVs);
-                ecp->c         = interpolate_property_value(elementVs, theAlphaCohes);
 
-                //ecp->c         = edata->sigma_0 * 0.2;
+            	sigma_c = 1000 * 0.1060 * pow( edata->Vs, 1.470 ) ;  // preconsolidation ratio (Pa)
+            	OCR     = sigma_c/edata->sigma_0;                    // Overconsolidation ratio
+
+            	if ( OCR > 4.0 )
+            		OCR = 4.0;
+
+            	if ( edata->Vs <= 200.0 )
+            		PIn = 10.0;
+            	else if ( edata->Vs > 200.0 &&  edata->Vs <= 360.0  )
+            		PIn = 5.0;
+            	else
+            		PIn = 0.0;
+
+            	tau_max            = 1.20 * ( 0.28 * pow( OCR , 0.80 ) * 1.50 * edata->sigma_0 ); // I'm assuming Ko=0.5
+
+            	ecp->c             = sqrt(3.0) * tau_max / 2.0;
+                // ecp->c         = interpolate_property_value(elementVs, theAlphaCohes);
                 ecp->phi       = 0.0;
                 ecp->dil_angle = 0.0;
 
                 ecp->alpha     = 0.0;
                 ecp->beta      = 0.0;
                 ecp->gamma     = 0.0;
-
-                //get_h_m_from_G_Gmax(*ecp, edata->sigma_0, &ecp->m, &ecp->psi0, &ecp->c);
-
-                //fprintf(stderr,"m is %f and h is %f and Su is %f kPa \n",ecp->m, ecp->psi0, ecp->c/1000);
 
                 ecp->Sstrain0  = 0.0;
                 ecp->m         = interpolate_property_value(elementVs, theM);
